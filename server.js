2c99301c5b1c060ebc912f618b0ae90e335c5f43
renan <renanslivka@gmail.com>
2021-09-06 19:32:57 +0300

    connect to db add static data build routes

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan')
+const connectDB = require('./config/db')
 
+//Load env vars
 dotenv.config({path: './config/config.env'})
+
+connectDB();
+
+//Routes files
+const bootcamps = require('./routes/bootcamps')
 
 const app = express();
 
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.use('/api/v1/bootcamps', bootcamps);
+
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=> console.log(`Server running in ${PORT}`))
+const server = app.listen(PORT, () => console.log(`Server running in ${PORT}`))
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(() => process.exit(1))
+})
