63abbc8df2be15992adfd73ba45d5b7441175a51
renan <renanslivka@gmail.com>
2021-09-07 00:32:17 +0300

    handle specific error from db

--- middleware/error.js
+++ middleware/error.js
@@ -3,11 +3,22 @@ const errorHandler = (err, req, res, next) => {
     let error = {...err}
     error.message = err.message;
     //Lof to console for dev
-    console.log(err.stack.red)
+    console.log(err);
     //Mongoose bad ObjectId
+
     if (err.name === 'CastError') {
         const message = `Resource not found with id of ${err.value}`
-        error = new ErrorResponse(message,404)
+        error = new ErrorResponse(message, 404)
+    }
+
+    if (err.code === 11000) {
+        const message = `Duplicate field value`
+        error = new ErrorResponse(message, 400)
+    }
+
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(value => value.message);
+        error = new ErrorResponse(message, 400)
     }
 
     res.status(err.statusCode || 500).json({
