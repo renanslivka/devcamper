d2070159913fcfd344aa48b46c119a292359b67b
renan <renanslivka@gmail.com>
2021-09-07 19:41:38 +0300

    add geocoder.js for coordinate

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-//const geocoder = require('../utils/geocoder');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema(
     {
@@ -116,7 +116,7 @@ BootcampSchema.pre('save', function(next) {
     this.slug = slugify(this.name, { lower: true });
     next();
 });
-/*
+
 // Geocode & create location field
 BootcampSchema.pre('save', async function(next) {
     const loc = await geocoder.geocode(this.address);
@@ -135,7 +135,7 @@ BootcampSchema.pre('save', async function(next) {
     this.address = undefined;
     next();
 });
-
+/*
 // Cascade delete courses when a bootcamp is deleted
 BootcampSchema.pre('remove', async function(next) {
     console.log(`Courses being removed from bootcamp ${this._id}`);
