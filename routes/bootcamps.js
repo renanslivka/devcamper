546dd704379516f863ba4ec4189856979b6cc753
renan <renanslivka@gmail.com>
2021-09-08 12:23:20 +0300

    add geo coder and get bootcamps by radius

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp} = require('../controllers/bootcamps')
+const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampsInRadius} = require('../controllers/bootcamps')
+
+router
+    .route('/radius/:zipcode/:distance')
+    .get(getBootcampsInRadius);
 
 router
     .route('/')
