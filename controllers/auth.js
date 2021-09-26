e75656d8a2ceda64945bf91ca9aa7b16efdf3c39
renan <renanslivka@gmail.com>
2021-09-27 00:14:42 +0300

    Logout To Clear Token Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,38 +4,60 @@ const asyncHandler = require('../middleware/async');
 const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
 
-//@desc     Register user
-//@route    Post / api/v1/auth/register
-//@access   Public
+// @desc      Register user
+// @route     POST /api/v1/auth/register
+// @access    Public
 exports.register = asyncHandler(async (req, res, next) => {
     const {name, email, password, role} = req.body;
 
+    // Create user
     const user = await User.create({
-        name, email, password, role
+        name,
+        email,
+        password,
+        role,
+    });
+
+    // grab token and send to email
+    const confirmEmailToken = user.generateEmailConfirmToken();
+
+    // Create reset url
+    const confirmEmailURL = `${req.protocol}://${req.get(
+        'host',
+    )}/api/v1/auth/confirmemail?token=${confirmEmailToken}`;
+
+    const message = `You are receiving this email because you need to confirm your email address. Please make a GET request to: \n\n ${confirmEmailURL}`;
+
+    user.save({validateBeforeSave: false});
+
+    const sendResult = await sendEmail({
+        email: user.email,
+        subject: 'Email confirmation token',
+        message,
     });
 
     sendTokenResponse(user, 200, res);
 });
 
-//@desc     Login user
-//@route    Post / api/v1/auth/login
-//@access   Public
+// @desc      Login user
+// @route     POST /api/v1/auth/login
+// @access    Public
 exports.login = asyncHandler(async (req, res, next) => {
     const {email, password} = req.body;
 
-    //Validate email & password
+    // Validate emil & password
     if (!email || !password) {
         return next(new ErrorResponse('Please provide an email and password', 400));
     }
 
-    //Check for user
+    // Check for user
     const user = await User.findOne({email}).select('+password');
 
     if (!user) {
         return next(new ErrorResponse('Invalid credentials', 401));
     }
 
-    //Check if password matches
+    // Check if password matches
     const isMatch = await user.matchPassword(password);
 
     if (!isMatch) {
@@ -43,6 +65,21 @@ exports.login = asyncHandler(async (req, res, next) => {
     }
 
     sendTokenResponse(user, 200, res);
+});
+
+// @desc      Log user out / clear cookie
+// @route     GET /api/v1/auth/logout
+// @access    Public
+exports.logout = asyncHandler(async (req, res, next) => {
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        data: {},
+    });
 });
 
 // @desc      Get current logged in user
@@ -59,7 +96,7 @@ exports.getMe = asyncHandler(async (req, res, next) => {
 });
 
 // @desc      Update user details
-// @route     PUT /api/v1/auth/update_details
+// @route     PUT /api/v1/auth/updatedetails
 // @access    Private
 exports.updateDetails = asyncHandler(async (req, res, next) => {
     const fieldsToUpdate = {
@@ -79,7 +116,7 @@ exports.updateDetails = asyncHandler(async (req, res, next) => {
 });
 
 // @desc      Update password
-// @route     PUT /api/v1/auth/update_password
+// @route     PUT /api/v1/auth/updatepassword
 // @access    Private
 exports.updatePassword = asyncHandler(async (req, res, next) => {
     const user = await User.findById(req.user.id).select('+password');
@@ -96,7 +133,7 @@ exports.updatePassword = asyncHandler(async (req, res, next) => {
 });
 
 // @desc      Forgot password
-// @route     POST /api/v1/auth/forgot_password
+// @route     POST /api/v1/auth/forgotpassword
 // @access    Public
 exports.forgotPassword = asyncHandler(async (req, res, next) => {
     const user = await User.findOne({email: req.body.email});
@@ -113,7 +150,7 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     // Create reset url
     const resetUrl = `${req.protocol}://${req.get(
         'host',
-    )}/api/v1/auth/reset_password/${resetToken}`;
+    )}/api/v1/auth/resetpassword/${resetToken}`;
 
     const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
 
@@ -137,13 +174,13 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 });
 
 // @desc      Reset password
-// @route     PUT /api/v1/auth/reset_password/:resetToken
+// @route     PUT /api/v1/auth/resetpassword/:resettoken
 // @access    Public
 exports.resetPassword = asyncHandler(async (req, res, next) => {
     // Get hashed token
     const resetPasswordToken = crypto
         .createHash('sha256')
-        .update(req.params.resetToken)
+        .update(req.params.resettoken)
         .digest('hex');
 
     const user = await User.findOne({
@@ -161,6 +198,46 @@ exports.resetPassword = asyncHandler(async (req, res, next) => {
     user.resetPasswordExpire = undefined;
     await user.save();
 
+    sendTokenResponse(user, 200, res);
+});
+
+/**
+ * @desc    Confirm Email
+ * @route   GET /api/v1/auth/confirmemail
+ * @access  Public
+ */
+exports.confirmEmail = asyncHandler(async (req, res, next) => {
+    // grab token from email
+    const {token} = req.query;
+
+    if (!token) {
+        return next(new ErrorResponse('Invalid Token', 400));
+    }
+
+    const splitToken = token.split('.')[0];
+    const confirmEmailToken = crypto
+        .createHash('sha256')
+        .update(splitToken)
+        .digest('hex');
+
+    // get user by token
+    const user = await User.findOne({
+        confirmEmailToken,
+        isEmailConfirmed: false,
+    });
+
+    if (!user) {
+        return next(new ErrorResponse('Invalid Token', 400));
+    }
+
+    // update confirmed to true
+    user.confirmEmailToken = undefined;
+    user.isEmailConfirmed = true;
+
+    // save
+    user.save({validateBeforeSave: false});
+
+    // return token
     sendTokenResponse(user, 200, res);
 });
 
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const express = require('express');
 const {
     register,
     login,
+    logout,
     getMe,
     forgotPassword,
     resetPassword,
@@ -16,6 +17,7 @@ const {protect} = require('../middleware/auth')
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
 router.get('/me', protect, getMe);
 router.put('/update_details', protect, updateDetails);
 router.put('/update_password', protect, updatePassword);
