02eaa4c0063b57ce8c39d33af353fcf8eaa66931
renan <renanslivka@gmail.com>
2021-09-26 23:28:36 +0300

    Add Review For Bootcamp

--- models/Review.js
+++ models/Review.js
@@ -32,10 +32,10 @@ const ReviewSchema = new mongoose.Schema({
         required: true
     }
 });
-/*
+
 // Prevent user from submitting more than one review per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
-
+/*
 // Static method to get avg rating and save
 ReviewSchema.statics.getAverageRating = async function(bootcampId) {
     const obj = await this.aggregate([
